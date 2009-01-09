6d983ff19ba92abb2a6955b6edf8dfb56ba58482
Adam Williams <adam@thewilliams.ws>
2009-01-09 15:30:31 -0500

    Fixed login field to be clear. Public layout for login. Linking to login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
-  # render new.rhtml
-  def new
-  end
-
+  layout 'public'
+  
   def create
     logout_keeping_session!
     account = Account.authenticate(params[:login], params[:password])
