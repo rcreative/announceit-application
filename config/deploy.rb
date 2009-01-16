5a0d1bea0e59e5d4acecf5722d157c4119922df1
Adam Williams <adam@thewilliams.ws>
2009-01-16 16:38:11 -0500

    Going to be using mysql in production. Resolve restarting for passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,15 +14,25 @@ role :app, "173.45.235.36"
 role :web, "173.45.235.36"
 role :db,  "173.45.235.36", :primary => true
 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
 
 namespace :slicehost do
-  
   desc "Configure VHost"
   task :config_vhost do
     vhost_config =<<-EOF
 <VirtualHost *:80>  
   ServerName 173.45.235.36
-  DocumentRoot #{deploy_to}/public
+  DocumentRoot #{current_path}/public
 </VirtualHost>
     EOF
     put vhost_config, "src/vhost_config"
@@ -34,5 +44,4 @@ namespace :slicehost do
   task :apache_reload do
     sudo "/etc/init.d/apache2 reload"
   end
-  
 end
