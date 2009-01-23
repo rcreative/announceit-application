740dd08425aa46990ad3455e7eb51623cc1eaf9f
John W. Long <me@johnwlong.com>
2009-01-23 12:58:18 -0500

    design: Allow a client to configure the subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
-
+  
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
-
+  
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
-
+  
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
-
+  
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
+  
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
   
@@ -23,23 +23,17 @@ ActionController::Routing::Routes.draw do |map|
   #     products.resources :comments
   #     products.resources :sales, :collection => { :recent => :get }
   #   end
-
+  
   # Sample resource route within a namespace:
   #   map.namespace :admin do |admin|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
-
+  
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-
+  
   # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-  # map.connect ':controller/:action/:id'
-  # map.connect ':controller/:action/:id.:format'
   
   map.connect '/', :controller => 'welcome', :action => 'index',
     :conditions => { :subdomain => /\A(ai)?\Z/ }
@@ -48,17 +42,23 @@ ActionController::Routing::Routes.draw do |map|
     :conditions => { :subdomain => Account.subdomain_regex }
   
   map.unimplemented '/unimplemented', :controller => 'welcome', :action => 'unimplemented'
-
+  
   map.signup '/signup', :controller => 'accounts', :action => 'new'
   map.login  '/login',  :controller => 'sessions', :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   
   map.namespace :admin, :path_prefix => "" do |admin|
-    admin.resource :settings
+    admin.resource :settings, :member => { :change_domain => [:get, :post] }
     admin.resources :subscribers
     admin.resource :statistics
   end
   
   map.resources :accounts
   map.resource :session
+  
+  # Install the default routes as the lowest priority.
+  # Note: These default routes make all actions in every controller accessible via GET requests. You should
+  # consider removing the them or commenting them out if you're using named routes and resources.
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
