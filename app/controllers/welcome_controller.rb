0535d2e85a957a3a50e13692ed8120456ac8c390
John W. Long <me@johnwlong.com>
2009-01-26 20:46:07 -0500

    redirect to www in production

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,11 @@ class WelcomeController < ApplicationController
   layout "public"
   
   def index
-    redirect_to admin_settings_path if logged_in?
+    case
+    when logged_in?
+      redirect_to admin_settings_path
+    when Rails.env.production? && (request.host_with_port !~ %r{^www\.})
+      redirect_to "http://www.#{request.host_with_port}"
+    end
   end
 end
