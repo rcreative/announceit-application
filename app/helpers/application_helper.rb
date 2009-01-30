8a120a2505faaba20b0d4b3141ed9eb041fc21d9
John W. Long <me@johnwlong.com>
2009-01-30 16:09:36 -0500

    gravatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,31 @@ module ApplicationHelper
     end
   end
   
+  # Returns a Gravatar URL associated with the email parameter.
+  # See: http://douglasfshearer.com/blog/gravatar-for-ruby-and-ruby-on-rails
+  def gravatar_url(email, gravatar_options={})
+    # Default highest rating.
+    # Rating can be one of G, PG, R X.
+    # If set to nil, the Gravatar default of G will be used.
+    gravatar_options[:rating] ||= "G"
+    
+    # Default size of the image.
+    # If set to nil, the Gravatar default size of 32px will be used.
+    gravatar_options[:size] ||= "32px"
+    
+    # Default image url to be used when no gravatar is found
+    # or when an image exceeds the rating parameter.
+    gravatar_options[:default] ||= '/images/avatar_32x32.png'
+    
+    # Build the Gravatar url.
+    grav_url = 'http://www.gravatar.com/avatar.php?'
+    grav_url << "gravatar_id=#{Digest::MD5.new.update(email)}" 
+    grav_url << "&rating=#{gravatar_options[:rating]}" if gravatar_options[:rating]
+    grav_url << "&size=#{gravatar_options[:size]}" if gravatar_options[:size]
+    grav_url << "&default=#{gravatar_options[:default]}" if gravatar_options[:default]
+    return grav_url
+  end
+  
   def teaser_host(account)
     "#{account.subdomain}.#{application_domain}"
   end
