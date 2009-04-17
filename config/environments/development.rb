3bdec58be5df9a93ee7b406f367a22b6cb8c8118
Adam Williams <adam@thewilliams.ws>
2009-04-17 14:13:09 -0400

    Fixed bug that caused app to not run so good on passenger locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,5 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-config.announce.tlds << 'localhost'
+config.announce.tlds << 'localhost'      # script/server
+config.announce.tlds << 'announce.local' # Phusion
