c93438e37811e47842e13f0b63ebf72c7ba20d58
Adam Williams <adam@thewilliams.ws>
2009-04-17 23:31:26 -0400

    Ready for customized templates without hosted images

--- db/migrate/20090417191350_add_customizable_builtin_template.rb
+++ db/migrate/20090417191350_add_customizable_builtin_template.rb
@@ -1,18 +1,33 @@
 class AddCustomizableBuiltinTemplate < ActiveRecord::Migration
-  class Teaser < ActiveRecord::Base
-  end
-  
   def self.up
     add_column :builtin_templates, :default_customizable, :boolean, :default => false
     add_index :builtin_templates, :default_customizable
     
     data = YAML.load(File.read(File.join(Rails.root, 'db', 'builtin_templates.yml')))
+    
+    Template.find(1).update_attributes!(
+      :source => data['White Background']['source'],
+      :styles => data['White Background']['styles']
+    )
+    
+    Template.find(2).update_attributes!(
+      :source => data['Dark Background']['source'],
+      :styles => data['Dark Background']['styles']
+    )
+    
     template = Template.create!(
       :name => 'Custom Template…',
       :source => data['Custom Template…']['source'],
       :styles => data['Custom Template…']['styles']
     )
     BuiltinTemplate.create!(:template => template, :default_customizable => true)
+    
+    
+    data = YAML.load(File.read(File.join(Rails.root, 'db', 'missioncontrol_template.yml')))
+    Template.find(3).update_attributes!(
+      :source => data['source'],
+      :styles => data['styles']
+    )
     
     Teaser.all(:conditions => {:template_id => nil}).each do |teaser|
       teaser.update_attribute(:template_id => 1) # White
