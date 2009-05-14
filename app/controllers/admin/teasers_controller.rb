56a3f6b5dba83324caf0f1b68e36c6c85ef289d5
Adam Williams <adam@thewilliams.ws>
2009-05-14 07:57:46 -0400

    Fixed bug where teaser logo would be deleted if it were not modified during an update to text

--- app/controllers/admin/teasers_controller.rb
+++ app/controllers/admin/teasers_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class TeasersController < AbstractController
     def update
+      params[:teaser].delete(:logo) if params[:teaser][:logo].blank?
       success = @account.update_attributes(params[:account])
       success &= @teaser.update_attributes(params[:teaser])
       if @teaser.custom_template_selected?
